651881ec0d7920b59d7d0685ffb331b3e388a1ca
Nana Kwame <9naquame@gmail.com>
2022-06-21 07:33:30 -0230

    added providers file

---
+++ providers.tf
@@ -0,0 +1,23 @@
+####################################
+# Terraform config
+####################################
+
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "~> 4.19.0"
+    }
+  }
+}
+
+
+
+####################################
+# Providers
+####################################
+
+provider "aws" {
+  # Configuration options
+  region = var.aws_region
+}
