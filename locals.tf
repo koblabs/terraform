4b43723cd768f139ea44661b0bd374dcf0995f0f
Nana Kwame <9naquame@gmail.com>
2022-06-27 02:47:23 -0230

    updated locals and network

--- locals.tf
+++ locals.tf
@@ -12,4 +12,16 @@ locals {
   }
 
   name_tag = "${var.naming_prefix}-${terraform.workspace}"
+
+  ingress = [{
+    cidr_blocks = [var.vpc_cidr_block]
+    description = "Allow incoming HTTP traffic from specified cidr blocks"
+    port        = 80
+    protocol    = "tcp"
+    }, {
+    cidr_blocks = ["0.0.0.0/0"]
+    description = "Allow incoming SSH traffic from anywhere"
+    port        = 22
+    protocol    = "tcp"
+  }]
 }
--- network.tf
+++ network.tf
@@ -65,27 +65,21 @@ resource "aws_security_group" "servers_sg" {
   description = "Security group settings for all servers"
   vpc_id      = module.vpc.vpc_id
 
-  ingress = [{
-    cidr_blocks      = [var.vpc_cidr_block]
-    description      = "Allow incoming HTTP traffic from specified cidr blocks"
-    from_port        = 80
-    ipv6_cidr_blocks = []
-    prefix_list_ids  = []
-    protocol         = "tcp"
-    security_groups  = []
-    self             = true
-    to_port          = 80
-    }, {
-    cidr_blocks      = ["0.0.0.0/0"]
-    description      = "Allow incoming SSH traffic from anywhere"
-    from_port        = 22
-    ipv6_cidr_blocks = ["::/0"]
-    prefix_list_ids  = []
-    protocol         = "tcp"
-    security_groups  = []
-    self             = true
-    to_port          = 22
-  }]
+
+  dynamic "ingress" {
+    for_each = local.ingress
+    content {
+      cidr_blocks      = ingress.value.cidr_blocks
+      description      = ingress.value.description
+      from_port        = ingress.value.port
+      ipv6_cidr_blocks = []
+      prefix_list_ids  = []
+      protocol         = ingress.value.protocol
+      security_groups  = []
+      self             = true
+      to_port          = ingress.value.port
+    }
+  }
 
   egress = [{
     cidr_blocks      = ["0.0.0.0/0"]
