f2e7ba09070fb87065d62ee95e759e688f63366a
Nana Kwame <9naquame@gmail.com>
2022-06-29 08:51:10 -0230

    vpc_id and vpc_arn added to outputs

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@
 # Output Setup
 ################################
 
+output "vpc_id" {
+  value = module.vpc.vpc_id
+}
+
+output "vpc_arn" {
+  value = module.vpc.vpc_arn
+}
+
 output "public_ip" {
   value = aws_instance.server[*].public_ip
 }
