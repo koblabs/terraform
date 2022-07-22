9c1503d646c514f40c144d4a7bc190c65ff08bf8
Nana Kwame <9naquame@gmail.com>
2022-07-22 07:02:27 -0230

    updated outputs

--- outputs.tf
+++ outputs.tf
@@ -2,18 +2,18 @@
 # Output Setup
 ################################
 
-# output "vpc_id" {
-#   value = module.vpc.vpc_id
-# }
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
 
-# output "vpc_arn" {
-#   value = module.vpc.vpc_arn
-# }
+output "vpc_arn" {
+  value = aws_vpc.main.arn
+}
 
 # output "private_ip" {
-#   value = aws_instance.server[*].private_ip
+#   value = aws_subnet.private.*.private_ip
 # }
 
-# output "s3_bucket_arn" {
-#   value = module.s3_bucket_for_logs.s3_bucket_arn
-# }
+output "s3_bucket_arn" {
+  value = module.s3_bucket_for_logs.s3_bucket_arn
+}
