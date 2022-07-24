e541f8938525c8e6e97fbae76e5889be661edd68
Nana Kwame <9naquame@gmail.com>
2022-07-24 09:37:12 -0230

    added instance

--- main.tf
+++ main.tf
@@ -2,23 +2,23 @@
 # Instances
 ################################
 
-# resource "aws_instance" "server" {
-#   count                  = var.instance_count
-#   ami                    = data.aws_ami.ami.id
-#   instance_type          = var.instance_type
-#   subnet_id              = module.vpc.private_subnets[(count.index % var.vpc_public_subnet_count)]
-#   vpc_security_group_ids = [aws_security_group.servers_sg.id]
-#   # iam_instance_profile   = module.web_app_s3.instance_profile.name
-#   # depends_on             = [module.web_app_s3]
+resource "aws_instance" "server" {
+  count                  = var.instance_count
+  ami                    = data.aws_ami.ami.id
+  instance_type          = var.instance_type
+  subnet_id              = element(aws_subnet.private.*.id, (count.index % var.vpc_private_subnet_count))
+  vpc_security_group_ids = [aws_security_group.servers_sg.id]
+  # iam_instance_profile   = module.web_app_s3.instance_profile.name
+  # depends_on             = [module.web_app_s3]
 
-#   # user_data = templatefile("${path.module}/startup_script.tpl", {
-#   #   s3_bucket_name = module.web_app_s3.web_bucket.id
-#   # })
+  # user_data = templatefile("${path.module}/startup_script.tpl", {
+  #   s3_bucket_name = module.web_app_s3.web_bucket.id
+  # })
 
-#   tags = merge(local.common_tags, {
-#     Name = "${local.name_tag}-server-${count.index}"
-#   })
-# }
+  tags = merge(local.common_tags, {
+    Name = "${local.name_tag}-server${count.index}"
+  })
+}
 
 
 ################################
@@ -50,7 +50,7 @@ module "s3_bucket_for_logs" {
 #   internal           = false
 #   load_balancer_type = "application"
 #   security_groups    = [aws_security_group.alb_sg.id]
-#   subnets            = module.vpc.public_subnets
+#   subnets            = aws_subnet.private.*.id
 
 #   enable_deletion_protection = false
 
@@ -60,14 +60,20 @@ module "s3_bucket_for_logs" {
 #     enabled = true
 #   }
 
-#   tags = local.common_tags
+#   tags = merge(local.common_tags, {
+#     Name = "${local.name_tag}-alb"
+#   })
 # }
 
 # resource "aws_lb_target_group" "nginx" {
 #   name     = "${local.name_tag}-alb-tg"
 #   port     = 80
 #   protocol = "HTTP"
-#   vpc_id   = module.vpc.vpc_id
+#   vpc_id   = aws_vpc.main.id
+
+#   tags = {
+#     Name = "${local.name_tag}-alb-tg"
+#   }
 # }
 
 # resource "aws_lb_listener" "nginx" {
