f9784d254531119cef234953f6bb8272d8f4fc96
Nana Kwame <9naquame@gmail.com>
2022-07-25 05:15:45 -0230

    load balancer on fleek

--- main.tf
+++ main.tf
@@ -45,49 +45,49 @@ module "s3_bucket_for_logs" {
 # # Load balancer
 # ################################
 
-# resource "aws_lb" "nginx" {
-#   name               = "${local.name_tag}-alb"
-#   internal           = false
-#   load_balancer_type = "application"
-#   security_groups    = [aws_security_group.alb_sg.id]
-#   subnets            = aws_subnet.private.*.id
+resource "aws_lb" "nginx" {
+  name               = "${local.name_tag}-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.alb_sg.id]
+  subnets            = aws_subnet.private.*.id
 
-#   enable_deletion_protection = false
+  enable_deletion_protection = false
 
-#   access_logs {
-#     bucket  = module.s3_bucket_for_logs.s3_bucket_id
-#     prefix  = "alb-logs"
-#     enabled = true
-#   }
+  access_logs {
+    bucket  = module.s3_bucket_for_logs.s3_bucket_id
+    prefix  = "alb-logs"
+    enabled = true
+  }
 
-#   tags = merge(local.common_tags, {
-#     Name = "${local.name_tag}-alb"
-#   })
-# }
+  tags = merge(local.common_tags, {
+    Name = "${local.name_tag}-alb"
+  })
+}
 
-# resource "aws_lb_target_group" "nginx" {
-#   name     = "${local.name_tag}-alb-tg"
-#   port     = 80
-#   protocol = "HTTP"
-#   vpc_id   = aws_vpc.main.id
+resource "aws_lb_target_group" "nginx" {
+  name     = "${local.name_tag}-alb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
 
-#   tags = {
-#     Name = "${local.name_tag}-alb-tg"
-#   }
-# }
+  tags = {
+    Name = "${local.name_tag}-alb-tg"
+  }
+}
 
-# resource "aws_lb_listener" "nginx" {
-#   load_balancer_arn = aws_lb.nginx.arn
-#   port              = "80"
-#   protocol          = "HTTP"
+resource "aws_lb_listener" "nginx" {
+  load_balancer_arn = aws_lb.nginx.arn
+  port              = "80"
+  protocol          = "HTTP"
 
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.nginx.arn
-#   }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.nginx.arn
+  }
 
-#   tags = local.common_tags
-# }
+  tags = local.common_tags
+}
 
 # resource "aws_lb_target_group_attachment" "nginx" {
 #   count            = var.instance_count
